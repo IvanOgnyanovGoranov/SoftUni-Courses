fd0892f7109fff0a66b275bad176f2c29fb740b4
IvanOgnyanovGoranov <144436457+IvanOgnyanovGoranov@users.noreply.github.com>
2025-05-04 06:54:18 +0300

    new models and functions

--- Python ORM/django_models_relations/caller.py
+++ Python ORM/django_models_relations/caller.py
@@ -1,14 +1,15 @@
 import os
 import django
-
-
+from django.db.models import QuerySet
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from main_app.models import Author, Book
+from main_app.models import Author, Book, Artist, Song
+
+
 # Create queries within functions
 
 
@@ -18,7 +19,7 @@ def show_all_authors_with_their_books() -> str:
     authors = Author.objects.all()
 
     for author in authors:
-        books = Book.objects.filter(author=author)
+        books = Book.objects.filter(author_id=author)
 
         if not books:
             continue
@@ -31,3 +32,25 @@ def show_all_authors_with_their_books() -> str:
 
 def delete_all_authors_without_books() -> None:
     Author.objects.filter(book__isnull=True).delete()
+
+
+
+def add_song_to_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
+
+
+def get_songs_by_artist(artist_name: str) -> QuerySet[Song]:
+    return Artist.objects.get(name=artist_name).songs.all().order_by('id')
+
+
+def remove_song_from_artist(artist_name: str, song_title: str) -> None:
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.remove(song)
+
+
+songs = get_songs_by_artist("Daniel Di Angelo")
--- Python ORM/django_models_relations/main_app/models.py
+++ Python ORM/django_models_relations/main_app/models.py
@@ -9,3 +9,14 @@ class Book(models.Model):
     title = models.CharField(max_length=40)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField(to=Song, related_name='artists')
+
+
